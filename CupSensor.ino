ef4137683de7e4fa4884a56ae73c07e4c04311a4
unknown <adammartinez86@gmail.com>
2020-08-04 14:57:39 -0700

    Updated pin selections

--- CupSensor.ino
+++ CupSensor.ino
@@ -4,8 +4,8 @@
 //2016.12.08
 #include "SR04.h"
 
-#define TRIG_PIN 12
-#define ECHO_PIN 11 
+#define TRIG_PIN 6
+#define ECHO_PIN 7 
 #define LIGHT_PIN 3
 #define CUP_DISTANCE 7
 #define TIME_DELAY 250
